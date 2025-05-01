e14100c914cb3b460250c490af5c07b439223194
Michael Pontus <m.pontus@gmail.com>
2025-05-01 14:02:47 +0000

    Update example app

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,12 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { ExampleStepFunction } from './example-step-function';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { ExampleStepFunction } from "./example-step-function";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as logs from 'aws-cdk-lib/aws-logs';
-
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { CfnPipe } from "aws-cdk-lib/aws-pipes";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as logs from "aws-cdk-lib/aws-logs";
+import { SqsSfnDispatcher } from "../../lib/index";
 
 /**
  * 0. Singleton step function
@@ -20,62 +20,65 @@ export class ExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'Queue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const queue = new sqs.Queue(this, "Queue", {
+      visibilityTimeout: cdk.Duration.seconds(10),
     });
 
-    const sampleStepMachine = new ExampleStepFunction(this, 'SampleStepMachine');
+    const sampleStepMachine = new ExampleStepFunction(
+      this,
+      "SampleStepMachine"
+    );
 
-    const role = new iam.Role(this, 'Role', {
-      assumedBy: new iam.ServicePrincipal('pipes.amazonaws.com')
-    });
-    role.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
-        resources: [queue.queueArn],
-      }),
-    )
-    role.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ["states:StartExecution", "states:StartSyncExecution"],
-        resources: [sampleStepMachine.stateMachine.stateMachineArn],
-      })
-    )
-
-    const logGroup = new logs.LogGroup(this, 'LogGroup', {
-      retention: logs.RetentionDays.TWO_WEEKS,
+    const dispatcher = new SqsSfnDispatcher(this, "SqsSfnDispatcher", {
+      source: queue,
+      target: sampleStepMachine.stateMachine,
     });
 
-    new CfnPipe(this, 'Pipe', {
-      roleArn: role.roleArn,
-      source: queue.queueArn,
-      sourceParameters: {
-        sqsQueueParameters: {
-          batchSize: 1
-        }
-      },
-      target: sampleStepMachine.stateMachine.stateMachineArn,
-      targetParameters: {
-        stepFunctionStateMachineParameters: {
-          invocationType: "FIRE_AND_FORGET",
-        }
-      },
-      logConfiguration: {
-        cloudwatchLogsLogDestination: {
-          logGroupArn: logGroup.logGroupArn,
-        },
-        includeExecutionData: ['ALL'],
-        level: 'INFO',
-      }
-    });
-      
+    // const role = new iam.Role(this, "Role", {
+    //   assumedBy: new iam.ServicePrincipal("pipes.amazonaws.com"),
+    // });
+    // role.addToPolicy(
+    //   new iam.PolicyStatement({
+    //     actions: [
+    //       "sqs:ReceiveMessage",
+    //       "sqs:DeleteMessage",
+    //       "sqs:GetQueueAttributes",
+    //     ],
+    //     resources: [queue.queueArn],
+    //   })
+    // );
+    // role.addToPolicy(
+    //   new iam.PolicyStatement({
+    //     actions: ["states:StartExecution", "states:StartSyncExecution"],
+    //     resources: [dispatcher.stateMachine.stateMachineArn],
+    //   })
+    // );
 
+    // const logGroup = new logs.LogGroup(this, "LogGroup", {
+    //   retention: logs.RetentionDays.TWO_WEEKS,
+    // });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // new CfnPipe(this, "Pipe", {
+    //   roleArn: role.roleArn,
+    //   source: queue.queueArn,
+    //   sourceParameters: {
+    //     sqsQueueParameters: {
+    //       batchSize: 1,
+    //     },
+    //   },
+    //   target: dispatcher.stateMachine.stateMachineArn,
+    //   targetParameters: {
+    //     stepFunctionStateMachineParameters: {
+    //       invocationType: "FIRE_AND_FORGET",
+    //     },
+    //   },
+    //   logConfiguration: {
+    //     cloudwatchLogsLogDestination: {
+    //       logGroupArn: logGroup.logGroupArn,
+    //     },
+    //     includeExecutionData: ["ALL"],
+    //     level: "INFO",
+    //   },
     // });
   }
 }
