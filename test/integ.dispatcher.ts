fd0254668b91df873fa9a5befc7715fb969b7882
Michael Pontus <m.pontus@gmail.com>
2025-05-03 15:14:15 +0000

    Test with single message

--- test/integ.dispatcher.ts
+++ test/integ.dispatcher.ts
@@ -1,8 +1,9 @@
 import { App, Stack, Duration } from "aws-cdk-lib";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
-import { IntegTest } from "@aws-cdk/integ-tests-alpha";
+import { ExpectedResult, IntegTest, Match } from "@aws-cdk/integ-tests-alpha";
 import { SqsSfnDispatcher } from "../lib/sqs-sfn-dispatcher";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 const app = new App();
 const stack = new Stack(app, "TestStack");
@@ -26,7 +27,6 @@ const dispatcher = new SqsSfnDispatcher(stack, "Dispatcher", {
 // Create the integration test
 const integ = new IntegTest(app, "DispatcherTest", {
   testCases: [stack],
-  diffAssets: true,
 });
 
 // Send test messages to the queue
@@ -37,39 +37,70 @@ const sendMessages = integ.assertions.awsApiCall("SQS", "sendMessageBatch", {
       Id: "1",
       MessageBody: JSON.stringify({ data: "message1" }),
     },
-    {
-      Id: "2",
-      MessageBody: JSON.stringify({ data: "message2" }),
-    },
-    {
-      Id: "3",
-      MessageBody: JSON.stringify({ data: "message3" }),
-    },
+    // {
+    //   Id: "2",
+    //   MessageBody: JSON.stringify({ data: "message2" }),
+    // },
+    // {
+    //   Id: "3",
+    //   MessageBody: JSON.stringify({ data: "message3" }),
+    // },
   ],
 });
 
-// // Start the dispatcher state machine
-// const startExecution = integ.assertions
-//   .awsApiCall("StepFunctions", "startExecution", {
-//     stateMachineArn: dispatcher.stateMachine.stateMachineArn,
-//   })
-//   .next(
-//     // Wait for execution to complete
-//     integ.assertions
-//       .awsApiCall("StepFunctions", "describeExecution", {
-//         executionArn: sfn.JsonPath.stringAt("$.executionArn"),
-//       })
-//       .waitForAssertions({
-//         timeout: Duration.seconds(30),
-//         assertions: [
-//           {
-//             assertionType: "objectLike",
-//             actual: sfn.JsonPath.stringAt("$.status"),
-//             expected: "SUCCEEDED",
-//           },
-//         ],
-//       })
-//   );
+sendMessages.provider.addPolicyStatementFromSdkCall("sqs", "sendMessage", [
+  queue.queueArn,
+]);
+
+// Start the dispatcher state machine
+const startExecution = integ.assertions.awsApiCall(
+  "StepFunctions",
+  "startExecution",
+  {
+    stateMachineArn: dispatcher.stateMachine.stateMachineArn,
+  }
+);
+const describeExecution = integ.assertions
+  .awsApiCall("StepFunctions", "describeExecution", {
+    executionArn: startExecution.getAttString("executionArn"),
+  })
+  .expect(
+    ExpectedResult.objectLike({
+      status: "SUCCEEDED",
+    })
+  )
+  .waitForAssertions({
+    totalTimeout: Duration.minutes(1),
+  });
+
+startExecution.next(describeExecution);
+
+const listExecutions = integ.assertions.awsApiCall(
+  "StepFunctions",
+  "listExecutions",
+  {
+    stateMachineArn: targetStateMachine.stateMachineArn,
+  }
+);
+
+// Assert that the number of executions is the same as the number of messages
+listExecutions.expect(
+  ExpectedResult.objectLike({
+    executions: [
+      {
+        status: "SUCCEEDED",
+      },
+      // {
+      //   status: "SUCCEEDED",
+      // },
+      // {
+      //   status: "SUCCEEDED",
+      // },
+    ],
+  })
+);
+
+describeExecution.next(listExecutions);
 
 // // Chain the assertions
 // sendMessages.next(startExecution);
