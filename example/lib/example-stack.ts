14bae306b5370ec3e6f36ec68cfa245b424f0d50
Michael Pontus <m.pontus@gmail.com>
2025-05-19 18:31:06 +0000

    Cleanup example app

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,20 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ExampleStepFunction } from "./example-step-function";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as sqs from "aws-cdk-lib/aws-sqs";
-import { CfnPipe } from "aws-cdk-lib/aws-pipes";
-import * as iam from "aws-cdk-lib/aws-iam";
-import * as logs from "aws-cdk-lib/aws-logs";
-import { SqsSfnDispatcher } from "../../lib/index";
-
-/**
- * 0. Singleton step function
- * 1. Shape of input from SQS
- * 2. Invoking different step function for each message
- * 3. Error handling
- * 9. integ tests
- */
+import { SqsStepFunctionDispatcher } from "../../lib/index";
 
 export class ExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -30,56 +18,9 @@ export class ExampleStack extends cdk.Stack {
       "SampleStepMachine"
     );
 
-    const dispatcher = new SqsSfnDispatcher(this, "SqsSfnDispatcher", {
+    const dispatcher = new SqsStepFunctionDispatcher(this, "Dispatcher", {
       source: queue,
       target: sampleStepMachine.stateMachine,
     });
-
-    // const role = new iam.Role(this, "Role", {
-    //   assumedBy: new iam.ServicePrincipal("pipes.amazonaws.com"),
-    // });
-    // role.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     actions: [
-    //       "sqs:ReceiveMessage",
-    //       "sqs:DeleteMessage",
-    //       "sqs:GetQueueAttributes",
-    //     ],
-    //     resources: [queue.queueArn],
-    //   })
-    // );
-    // role.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     actions: ["states:StartExecution", "states:StartSyncExecution"],
-    //     resources: [dispatcher.stateMachine.stateMachineArn],
-    //   })
-    // );
-
-    // const logGroup = new logs.LogGroup(this, "LogGroup", {
-    //   retention: logs.RetentionDays.TWO_WEEKS,
-    // });
-
-    // new CfnPipe(this, "Pipe", {
-    //   roleArn: role.roleArn,
-    //   source: queue.queueArn,
-    //   sourceParameters: {
-    //     sqsQueueParameters: {
-    //       batchSize: 1,
-    //     },
-    //   },
-    //   target: dispatcher.stateMachine.stateMachineArn,
-    //   targetParameters: {
-    //     stepFunctionStateMachineParameters: {
-    //       invocationType: "FIRE_AND_FORGET",
-    //     },
-    //   },
-    //   logConfiguration: {
-    //     cloudwatchLogsLogDestination: {
-    //       logGroupArn: logGroup.logGroupArn,
-    //     },
-    //     includeExecutionData: ["ALL"],
-    //     level: "INFO",
-    //   },
-    // });
   }
 }
